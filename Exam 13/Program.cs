fd37957d118e43cfae2a23b899bd857d8e4ac72c
VadymKucherenko <VadymKucherenko@users.noreply.github.com>
2021-06-03 10:30:09 +0300

    Добавьте файлы проекта.

---
+++ Exam 13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_13
+{
+    class Program
+    {
+        static void Calculate(int a, int b, int c)
+        {
+            Console.WriteLine("Среднє арифметичне: " + ((a + b + c) / 3).ToString());
+            Console.ReadKey();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Введить 1 зачення");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введить 2 значення");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введить 3 значення");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Calculate(a, b, c);
+        }
+
+    }
+}
